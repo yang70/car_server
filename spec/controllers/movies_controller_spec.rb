cae44e8791f866159912eb3bd144ebf71e5d9969
Matthew Yang <yang70@gmail.com>
2016-01-10 09:03:16 -0800

    passing movie tests

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -8,13 +8,13 @@ RSpec.describe MoviesController, type: :controller do
       expect(response).to have_http_status(200)
     end
 
-    xit "renders the index template" do
+    it "renders the index template" do
       get :index
       expect(response).to render_template("index")
     end
 
-    xit "loads all of the movies into @movies" do
-      movie1, movie1 = Movie.create!(title: "Movie One"), Movie.create!(title: "Movie Two")
+    it "loads all of the movies into @movies" do
+      movie1, movie2 = Movie.create!(title: "Movie One"), Movie.create!(title: "Movie Two")
       get :index
       expect(assigns(:movies)).to match_array([movie1, movie2])
     end
